c49e5dcd1ef7f4fab52a3554d9f9e2814234bfe1
Andre Silva <andremarciliosilva@gmail.com>
2025-04-14 20:08:20 -0300

    Versão final dos exercícios

--- exercicios.py
+++ exercicios.py
@@ -1,13 +1,19 @@
-### Exercício 1: Verificação de Qualidade de Dados
-# Você está analisando um conjunto de dados de vendas e precisa garantir 
-# que todos os registros tenham valores positivos para `quantidade` e `preço`. 
-# Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
-# forem positivos ou "Dados inválidos" caso contrário.
-'''
+
+
+print('''Exercício 1: Verificação de Qualidade de Dados
+Você está analisando um conjunto de dados de vendas e precisa garantir 
+que todos os registros tenham valores positivos para `quantidade` e `preço`. 
+Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
+forem positivos ou "Dados inválidos" caso contrário.''')
+print ('\n')
+
+print('Lendo arquivo csv')
+print('Resultado:')
+
 import csv
 dict: list = []
 
-# Primeira resolução
+print('Primeira resolução')
 with open('dados.csv', 'r') as csvdados:
 
     for row in csvdados:
@@ -22,7 +28,7 @@ with open('dados.csv', 'r') as csvdados:
                 print('Dados inválidos')
                 break
 
-# Segunda resolução
+print('Segunda resolução')
 with open('dados.csv', 'r') as csvdados:
     reader = csv.DictReader(csvdados)
 
@@ -38,195 +44,258 @@ for ch in dict:
     else:
         print('Dados inválidos')
         break
-'''
 
+print ('\n')
+print('''Exercício 2: Classificação de Dados de Sensor
+Imagine que você está trabalhando com dados de sensores IoT. 
+Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
+como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+Temperatura < 18°C é 'Baixa'
+Temperatura >= 18°C e <= 26°C é 'Normal'
+Temperatura > 26°C é 'Alta'
+Temperatura inserida para teste: 17°C
+''')
+print ('\n')
 
-### Exercício 2: Classificação de Dados de Sensor
-# Imagine que você está trabalhando com dados de sensores IoT. 
-# Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
-# como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
-# Temperatura < 18°C é 'Baixa'
-# Temperatura >= 18°C e <= 26°C é 'Normal'
-# Temperatura > 26°C é 'Alta'
+temp_baixa = 18
+temp_normal = range(18,26)
+temp_alta = 30
 
-# temp_baixa = 18
-# temp_normal = range(18,26)
-# temp_alta = 30
+temp_atual = 17
 
-# temp_atual = 17
+if temp_atual < 18:
+    print('A temperatura está baixa')
 
-# if temp_atual < 18:
-#     print('A temperatura está baixa')
+elif temp_atual in temp_normal:
+    print('A temperatura está normal')
 
-# elif temp_atual in temp_normal:
-#     print('A temperatura está normal')
+else:
+    print('A temperatura está alta')
 
-# else:
-#     print('A temperatura está alta')
+print ('\n')
+print('''Exercício 3: Filtragem de Logs por Severidade
+Você está analisando logs de uma aplicação e precisa filtrar mensagens 
+com severidade 'ERROR'. Dado um registro de log em formato de dicionário 
+como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
+escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+''')
+print ('\n')
 
-### Exercício 3: Filtragem de Logs por Severidade
-# Você está analisando logs de uma aplicação e precisa filtrar mensagens 
-# com severidade 'ERROR'. Dado um registro de log em formato de dicionário 
-# como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
-# escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
 
-# log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
+for i in log.values():
+    if i == 'ERROR':
+        print(i)
 
-# for i in log.values():
-#     if i == 'ERROR':
-#         print(i)
+print ('\n')
+print ('''Exercício 4: Validação de Dados de Entrada
+Antes de processar os dados de usuários em um sistema de recomendação, 
+você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha 
+fornecido um email válido. Escreva um programa que valide essas condições 
+e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+Dados inserivos para teste:
+idade = 30
+email = 'andresilva@gmail.com'
+''')
+print ('\n')
+idade = 30
+email = 'andresilva@gmail.com'
 
-### Exercício 4: Validação de Dados de Entrada
-# Antes de processar os dados de usuários em um sistema de recomendação, 
-# você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha 
-# fornecido um email válido. Escreva um programa que valide essas condições 
-# e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+if idade in range(18,65) and '@' in email:
+    print('Dados de usuário válidos')
+else:
+    print('Dados de usuário inválidos')
 
-# idade = 30
-# email = 'andresilva@gmail.com'
+print ('\n')
+print(''' Exercício 5: Detecção de Anomalias em Dados de Transações
+Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
+transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
+a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
+Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
+''')
+print ('\n')
 
-# if idade in range(18,65) and '@' in email:
-#     print('Dados de usuário válidos')
-# else:
-#     print('Dados de usuário inválidos')
+transacao = {'valor': 10000, 'hora': 20}
 
+if transacao['valor'] > 10000 or transacao['hora'] not in range(9,19):
+    print('Nao ok')
+else:
+    print('OK')
 
-### Exercício 5: Detecção de Anomalias em Dados de Transações
-# Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
-# transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
-# a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
-# Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
+print ('\n')
+print('''Exercício 6. Contagem de Palavras em Textos
+Objetivo: Dado um texto, contar quantas vezes cada palavra única aparece nele.
+Texto inserido para teste: "a a a a a raposa marrom salta sobre o o o o cachorro preguiçoso"
+''')
+print ('\n')
 
-# transacao = {'valor': 10000, 'hora': 20}
+texto = "a a a a a raposa marrom salta sobre o o o o cachorro preguiçoso"
+lista: list = texto.split()
+contagem_palavras = {}
 
-# if transacao['valor'] > 10000 or transacao['hora'] not in range(9,19):
-#     print('Nao ok')
-# else:
-#     print('OK')
+print(lista)
 
-### Exercício 6. Contagem de Palavras em Textos
-# Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
-
-# texto = "a a a a a raposa marrom salta sobre o o o o cachorro preguiçoso"
-# lista: list = texto.split()
-# contagem_palavras = {}
-
-# print(lista)
-
-# for i in lista:
-#     if i in contagem_palavras:
-#         contagem_palavras[i] += 1
-#     else:
-#         contagem_palavras[i] = 1
+for i in lista:
+    if i in contagem_palavras:
+        contagem_palavras[i] += 1
+    else:
+        contagem_palavras[i] = 1
     
-# print(contagem_palavras)
+print(contagem_palavras)
 
-### Exercício 7. Normalização de Dados
-# Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+print ('\n')
+print ('''Exercício 7. Normalização de Dados
+Objetivo: Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+Lista de números inseridas para teste: numeros = [10, 20, 30, 40, 50]
+''')
+print ('\n')
 
-# numeros = [10, 20, 30, 40, 50]
-# minimo = min(numeros)
-# maximo = max(numeros)
-# normalizados = [(x - minimo) / (maximo - minimo) for x in numeros]
+numeros = [10, 20, 30, 40, 50]
+minimo = min(numeros)
+maximo = max(numeros)
+normalizados = [(x - minimo) / (maximo - minimo) for x in numeros]
 
-# print(normalizados)
+print(normalizados)
 
-### Exercício 8. Filtragem de Dados Faltantes
-# Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando.
+print ('\n')
+print('''Exercício 8. Filtragem de Dados Faltantes
+Objetivo: Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando.
+Dicionários inseridos para teste: 
+      usuarios = [
+    {"nome": "Alice", "email": "alice@example.com"},
+    {"nome": "Bob", "email": ""},
+    {"nome": "Carol", "email": "carol@example.com"}
+]
+''')
+print ('\n')
 
-# usuarios = [
-#     {"nome": "Alice", "email": "alice@example.com"},
-#     {"nome": "Bob", "email": ""},
-#     {"nome": "Carol", "email": "carol@example.com"}
-# ]
+usuarios = [
+    {"nome": "Alice", "email": "alice@example.com"},
+    {"nome": "Bob", "email": ""},
+    {"nome": "Carol", "email": "carol@example.com"}
+]
 
-# for d in usuarios:
-#     for k, v in d.items():
-#         if v == '':
-#             print(f'O usuário {d['nome']} tem campo faltando')
+for d in usuarios:
+    for k, v in d.items():
+        if v == '':
+            print(f'O usuário {d['nome']} tem campo faltando')
 
+print ('\n')
+print('''Exercício 9. Extração de Subconjuntos de Dados
+Objetivo: Dada uma lista de números, extrair apenas aqueles que são pares.
+Dados inseridos para teste: lista: list = range(1,30)
+''')
+print ('\n')
 
-# Exercício 9. Extração de Subconjuntos de Dados
-# Objetivo: Dada uma lista de números, extrair apenas aqueles que são pares.
+lista: list = range(1,30)
 
-# lista: list = range(1,30)
+for n in lista:
+    if n % 2 == 0:
+        print(n)
 
-# for n in lista:
-#     if n % 2 == 0:
-#         print(n)
+print ('\n')
+print('''Exercício 10. Agregação de Dados por Categoria.
+Objetivo: Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+Dados inseridos para teste:
+      vendas = [
+    {"categoria": "eletrônicos", "valor": 1200},
+    {"categoria": "livros", "valor": 200},
+    {"categoria": "eletrônicos", "valor": 800}
+]
+''')
+print ('\n')
 
+vendas = [
+    {"categoria": "eletrônicos", "valor": 1200},
+    {"categoria": "livros", "valor": 200},
+    {"categoria": "eletrônicos", "valor": 800}
+]
 
-# Exercício 10. Agregação de Dados por Categoria.
-# Objetivo: Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+vendas_categoria = {}
 
-# vendas = [
-#     {"categoria": "eletrônicos", "valor": 1200},
-#     {"categoria": "livros", "valor": 200},
-#     {"categoria": "eletrônicos", "valor": 800}
-# ]
+for v in vendas:
+    categoria = v['categoria']
+    valor = v['valor']
 
-# vendas_categoria = {}
+    if categoria in vendas_categoria:
+     vendas_categoria[categoria] += valor
 
-# for v in vendas:
-#     categoria = v['categoria']
-#     valor = v['valor']
+    else:
+      vendas_categoria[categoria] = valor
 
-#     if categoria in vendas_categoria:
-#      vendas_categoria[categoria] += valor
+print(vendas_categoria)
 
-#     else:
-#       vendas_categoria[categoria] = valor
+print ('\n')
+print('''Exercício 11. Leitura de Dados até Flag
+Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+''')
+print ('\n')
 
-# print(vendas_categoria)
+while True:
+    flag = input('Digite alguma coisa pra continuar ou sair pra sair do programa:')
+    if flag == 'sair':
+        break
 
-# Exercício 11. Leitura de Dados até Flag
-# Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+print ('\n')
+print('''Exercício 12. Validação de Entrada.
+Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+''')
+print ('\n')
 
-# while True:
-#     flag = input('Digite algo:')
-#     if flag == 'sair':
-#         break
+while True:
 
-# Exercício 12. Validação de Entrada.
-# Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+    entrada = int(input('Digite um número entre 0 e 10:'))
+    if entrada in range(0,11):
+        break
+    else:
+        continue
 
-# while True:
+print ('\n')
+print('''Exercício 13. Consumo de API Simulado.
+Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
+Dados inseridos para teste:
+pagina_atual = 0
+paginas_totais = 10
+''')
+print ('\n')
 
-#     entrada = int(input('Digite um número entre 0 e 10:'))
-#     if entrada in range(0,11):
-#         break
-#     else:
-#         continue
+pagina_atual = 0
+paginas_totais = 10
 
-# Exercício 13. Consumo de API Simulado.
-# Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
+while pagina_atual <= paginas_totais :
 
-# pagina_atual = 0
-# paginas_totais = 10
+    print(f'Consumindo pagina {pagina_atual} de {paginas_totais} paginas totais' )
+    pagina_atual += 1
 
-# while pagina_atual <= paginas_totais :
+print ('\n')
+print('''Exercício 14. Tentativas de Conexão
+Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+Dados inseridos para teste:
+tentativa = 0
+limite_tentativas = 15
+''')
+print ('\n')
 
-#     print(f'Consumindo pagina {pagina_atual} de {paginas_totais} paginas totais' )
-#     pagina_atual += 1
+tentativa = 0
+limite_tentativas = 15
 
-# Exercício 14. Tentativas de Conexão
-# Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+while tentativa <= limite_tentativas:
+    print(f'Tentativa de conexão {tentativa} de {limite_tentativas} tentativas')
+    tentativa += 1
 
-# tentativa = 0
-# limite_tentativas = 15
+print ('\n')
+print('''Exercício 15. Processamento de Dados com Condição de Parada
+Processar itens de uma lista até encontrar um valor específico que indica a parada.
+Dados inseridos para teste:
+itens = [1, 2, 3, "parar", 4, 5]
+''')
+print ('\n')
 
-# while tentativa <= limite_tentativas:
-#     print(f'Tentativa de conexão {tentativa} de {limite_tentativas} tentativas')
-#     tentativa += 1
+itens = [1, 2, 3, "parar", 4, 5]
 
-# Exercício 15. Processamento de Dados com Condição de Parada
-# Processar itens de uma lista até encontrar um valor específico que indica a parada.
-
-# itens = [1, 2, 3, "parar", 4, 5]
-
-# for i in itens:
-#     if i == 'parar':
-#         print('Valor encontrado')
-#         break
-#     else:
-#         print(i)
+for i in itens:
+    if i == 'parar':
+        print('Valor encontrado')
+        break
+    else:
+        print(i)
